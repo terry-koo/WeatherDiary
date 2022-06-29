d225001f4c6305d457cc68f78b3662d13cee0dd5
Terry-Koo <xpflxhfl94@naver.com>
2022-06-29 21:14:21 +0900

    [Docs] gitignore파일 추가 및 프로젝트 초기화

---
+++ WeatherDiary/WeatherDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDiaryApp.swift
+//  WeatherDiary
+//
+//  Created by Terry Koo on 2022/06/29.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
