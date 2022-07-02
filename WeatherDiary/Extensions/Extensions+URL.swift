0f93e38f9d178e624a62976e49910d8ed8644cf8
otoolz <otool.hf@gmail.com>
2022-07-02 17:48:28 +0900

    [Feat] #10 시간별 날씨를 위한 url 반환 함수 추가
    Extensions group
    Extensions+URL 추가
    시간별 날씨를 받기 위한  URL 반환 함수 추가

---
+++ WeatherDiary/Extensions/Extensions+URL.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions+URL.swift
+//  WeatherDiary
+//
+//  Created by KiWoong Hong on 2022/07/02.
+//
+
+import Foundation
+
+let API_KEY = "zfUHmCouE2vnx14zqmzMMoIb%2Fe2PYANk0aPgx7MStVXdIeeGcwoPOsxfDn38UsHCtkKCBqqW%2FgELFNmAedcTmQ%3D%3D"
+
+extension URL {
+    static func forHourlyWeather(_ date: String, _ base_time: String, _ nx: Int, _ ny: Int) -> URL? {
+        return URL(string: "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=\(API_KEY)&pageNo=1&numOfRows=240&dataType=JSON&base_date=\(date)&base_time=\(base_time)&nx=\(nx)&ny=\(ny)")
+    }
+}
