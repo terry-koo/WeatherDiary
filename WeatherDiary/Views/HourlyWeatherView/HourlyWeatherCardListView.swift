34d94fa5e338eafc7c3d85c330bd2fd83a6d1176
otoolz <otool.hf@gmail.com>
2022-07-07 12:14:33 +0900

    [Refactor] #11 dummy 데이터 리팩토링
    프리뷰에 사용하는 dummy데이터 메모리 최적화위해 리팩토링

--- WeatherDiary/Views/HourlyWeatherView/HourlyWeatherCardListView.swift
+++ WeatherDiary/Views/HourlyWeatherView/HourlyWeatherCardListView.swift
@@ -20,35 +20,8 @@ struct HourlyWeatherCardListView: View {
     }
 }
 
-let dummy: [HourlyWeather] = [
-    HourlyWeather(time: "지금", temperature: "28도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오전 1시", temperature: "26도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오전 2시", temperature: "27도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오전 3시", temperature: "24도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오전 4시", temperature: "26도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오전 5시", temperature: "27도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오전 6시", temperature: "24도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오전 7시", temperature: "26도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오전 8시", temperature: "28도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오전 9시", temperature: "27도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오전 10시", temperature: "24도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오전 11시", temperature: "26도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오전 12시", temperature: "27도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오후 1시", temperature: "24도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오후 2시", temperature: "26도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오후 3시", temperature: "24도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오후 4시", temperature: "27도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오후 5시", temperature: "28도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오후 6시", temperature: "24도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오후 7시", temperature: "24도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오후 8시", temperature: "27도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오후 9시", temperature: "26도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오후 10시", temperature: "27도", rainProbabillity: "0", condition: "0", sky: "2"),
-    HourlyWeather(time: "오후 11시", temperature: "28도", rainProbabillity: "0", condition: "0", sky: "2")
-]
-
 struct HourlyWeatherCardListView_Previews: PreviewProvider {
     static var previews: some View {
-        HourlyWeatherCardListView(hourlyWeathers: dummy)
+        HourlyWeatherCardListView(hourlyWeathers: HourlyWeatherManager().dummy)
     }
 }
