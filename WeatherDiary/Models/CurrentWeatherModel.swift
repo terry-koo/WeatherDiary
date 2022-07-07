c00cf2cd6a7daad208a8c97a56b3352a035ecbdc
Terry-Koo <xpflxhfl94@naver.com>
2022-07-07 23:29:36 +0900

    [Feat] #5 현재 기상상태를 보여주기 리턴해주는 condition 연산 프로퍼티 추가

--- WeatherDiary/Models/CurrentWeatherModel.swift
+++ WeatherDiary/Models/CurrentWeatherModel.swift
@@ -8,8 +8,37 @@
 import Foundation
 
 struct CurrentWeather {
-    var condition: String?
+    var condition: String {
+        if Int(self.pop!)! > 50 {
+            switch self.pty {
+            case "1":
+                return "비"
+            case "2":
+                return "비/눈"
+            case "3":
+                return "눈"
+            case "4":
+                return "소나기"
+            default:
+                return "없음"
+            }
+        } else {
+            switch Int(self.sky!)! {
+            case 0..<5:
+                return "맑음"
+            case 6..<8:
+                return "구름많음"
+            case 9..<10:
+                return "흐림"
+            default:
+                return "없음"
+            }
+        }
+    }
     var currentTemp: String?
     var highestTemp : String?
     var lowestTemp: String?
+    var sky: String?
+    var pty: String?
+    var pop: String?
 }
