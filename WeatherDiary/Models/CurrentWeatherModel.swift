67c933bae33f5a3f8d9f09eaa00ef12605493442
Terry-Koo <xpflxhfl94@naver.com>
2022-07-07 21:31:44 +0900

    [Feat] #5 기존 CurrentModel -> ResponseModel로 변경, 새로운 CurrentModel 추가

--- WeatherDiary/Models/CurrentWeatherModel.swift
+++ WeatherDiary/Models/CurrentWeatherModel.swift
@@ -2,42 +2,14 @@
 //  CurrentWeatherModel.swift
 //  WeatherDiary
 //
-//  Created by Terry Koo on 2022/07/03.
+//  Created by Terry Koo on 2022/07/07.
 //
 
 import Foundation
 
-// MARK: - Response
-struct Response: Codable {
-    let response: ResponseStruct
-}
-
-// MARK: - ResponseStruct
-struct ResponseStruct: Codable {
-    let header: Header
-    let body: Body
-}
-
-// MARK: - Body
-struct Body: Codable {
-    //let dataType: String
-    let items: Items
-    let pageNo, numOfRows, totalCount: Int
-}
-
-// MARK: - Items
-struct Items: Codable {
-    let item: [Item]
-}
-
-// MARK: - Item
-struct Item: Codable {
-    let baseDate, baseTime, category, fcstDate: String
-    let fcstTime, fcstValue: String
-    //let nx, ny: Int
-}
-
-// MARK: - Header
-struct Header: Codable {
-    let resultCode, resultMsg: String
+struct CurrentWeather {
+    var condition: String
+    var currentTemp: String
+    var highestTemp : String
+    var lowestTemp: String
 }
