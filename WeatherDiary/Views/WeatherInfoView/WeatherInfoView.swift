51c5cd53dce9749f31c0d378df4b4621e1ecb5b5
otoolz <otool.hf@gmail.com>
2022-11-07 19:22:06 +0900

    [fix] 오타 및 에러 수정
    오타 및 에러 수정

--- WeatherDiary/Views/WeatherInfoView/WeatherInfoView.swift
+++ WeatherDiary/Views/WeatherInfoView/WeatherInfoView.swift
@@ -6,13 +6,36 @@
 //
 
 import SwiftUI
+import WeatherKit
 
 struct WeatherInfoView: View {
+    
+    let weatherService = WeatherService.shared
+    @StateObject private var locationManager = LocationManager()
+    @State private var weather: Weather?
+    
     var body: some View {
         ScrollView {
             CurrentWeatherView()
             HourlyWeatherListView()
             WeeklyWeatherListView()
+            
+            // currentWeather: CurrentWeather = weather.currentWeather
+            // hourWeathers: [HourWeather] = weather.hourlyForecast.forecast
+            // dayWeathers: [DayWeather] = weather.dailyForecast.forecast
+            
+            // when changed locationManager.currentLocation
+            // redo task
+        }
+        .task(id: locationManager.currentLocation) {
+            do {
+                
+                if let location = locationManager.currentLocation {
+                    self.weather = try await weatherService.weather(for: location)
+                }
+            } catch {
+                print(error)
+            }
         }
     }
 }
