8e82363da58730c73764808c1ea9212cbe8432d0
otoolz <otool.hf@gmail.com>
2022-11-08 21:26:25 +0900

    [feat] 로딩 뷰 이미지 터치시 회전 추가
    로딩 뷰 이미지 터치시 회전 추가

--- WeatherDiary/Views/LoadingView.swift
+++ WeatherDiary/Views/LoadingView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct LoadingView: View {
+    
+    @State private var angle = 0.0
+    
     var body: some View {
         VStack {
             Spacer()
@@ -15,6 +18,10 @@ struct LoadingView: View {
             Image("sun")
                 .resizable()
                 .frame(width: 100, height: 100)
+                .rotationEffect(.degrees(angle))
+                .animation(.timingCurve(0, 0.8, 0.2, 1, duration: 10), value: angle)
+                .onTapGesture { angle += Double.random(in: 3600 ..< 3960) }
+            
             Text("엄마한테 날씨 물어보는 중...")
                 .font(Font.weatherTitle)
         
